2d4e359d3d381a7fdd16bbce0f923169ea42443f
alesteka <tekavcicales1@gmail.com>
2024-08-11 17:40:32 +0200

    g

--- main.tf
+++ main.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.2"
+      version = "4.21.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 module "s3-webapp" {
   source  = "app.terraform.io/terraform-app/s3-webapp/aws"
   name    = "ales"
-  region  = "us-east-1"
+  region  = "eu-central-1"
   prefix  = "dev-test"
   version = "1.0.0"
 }
